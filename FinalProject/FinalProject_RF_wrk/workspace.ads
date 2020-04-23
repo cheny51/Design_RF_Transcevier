6c37e706cbd1393f9ab778b9f949c4c9fed32efb
aiden <cheny51@rpi.edu>
2020-04-22 21:20:34 -0400

    Initial Folder

---
+++ FinalProject/FinalProject_RF_wrk/workspace.ads
@@ -0,0 +1,31 @@
+<ADSWorkspace Revision="2" Version="100">
+    <Workspace Name="">
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_verification_test_bench" />
+        <LibraryDefs Name="lib.defs" />
+        <Library Name="FinalProject_RF_lib" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Folder Name="LNA_AMPLIFIER">
+            <Cell Name="FinalProject_RF_lib:LNA_sketch" />
+        </Folder>
+    </Workspace>
+</ADSWorkspace>
